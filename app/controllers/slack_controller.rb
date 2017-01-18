395124d685a0d1a2cb2ee3d475816d7063636abc
Gerald Coulter-Mistele <gerald@prosperworks.com>
2017-01-18 06:58:41 -0800

    format close poll

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -32,10 +32,21 @@ class SlackController < ApplicationController
     poll.open = false
     poll.save
 
-    msg = {
-      "response_type" =>  "in_channel",
-      "text" => "Poll Closed: #{poll_name}"
-    }
+    winner_id = get_winner
+
+    if(winner_id == -1)
+      msg = {
+        "response_type" =>  "in_channel",
+        "text" => "Poll Closed: #{poll_name}"
+      }
+    else
+      winner = Candidate.find(winner_id)
+      
+      msg = {
+        "response_type" =>  "in_channel",
+        "text" => "Poll Closed: #{poll_name}, The Winner Is: #{winner.name}"
+      }
+    end
 
     render json: msg, status: :success
   end
@@ -108,43 +119,7 @@ class SlackController < ApplicationController
   end
 
   def see_winner
-    poll = get_poll
-
-    candidates = Candidate.where(poll_id: poll.id)
-
-    candidate_list = candidates.collect{|x| x.id}
-
-    winner_id = -1
-
-    while(candidate_list.length > 1) do
-      sql = "select candidate_id,count(*) from votes,
-              (
-              select voter, min(priority) as priority from votes
-              where
-                poll_id = #{poll.id} and
-                candidate_id in (#{candidate_list.join(",")})
-              group by voter
-              ) as best_vote
-            where
-              best_vote.voter = votes.voter and
-              best_vote.priority = votes.priority
-            group by candidate_id
-            order by count(*) DESC;"
-
-      records_array = ActiveRecord::Base.connection.execute(sql)
-      if(records_array.to_a.length == 0)
-        msg = {
-          "response_type" =>  "ephemeral",
-          "text" => "No Winner In: #{poll_name}"
-        }
-
-        render json: msg, status: :success
-        return
-      end
-      last = records_array.to_a[-1]
-      candidate_list.delete(last["candidate_id"].to_i)
-      winner_id = records_array.to_a[0]["candidate_id"].to_i
-    end
+    winner_id = get_winner
 
     if(winner_id == -1)
       msg = {
@@ -189,5 +164,41 @@ class SlackController < ApplicationController
         return
       end
       p.last
+    end
+
+    def get_winner
+      poll = get_poll
+
+      candidates = Candidate.where(poll_id: poll.id)
+
+      candidate_list = candidates.collect{|x| x.id}
+
+      winner_id = -1
+
+      while(candidate_list.length > 1) do
+        sql = "select candidate_id,count(*) from votes,
+              (
+              select voter, min(priority) as priority from votes
+              where
+                poll_id = #{poll.id} and
+                candidate_id in (#{candidate_list.join(",")})
+              group by voter
+              ) as best_vote
+            where
+              best_vote.voter = votes.voter and
+              best_vote.priority = votes.priority
+            group by candidate_id
+            order by count(*) DESC;"
+
+        records_array = ActiveRecord::Base.connection.execute(sql)
+        if(records_array.to_a.length == 0)
+          return -1
+        end
+        last = records_array.to_a[-1]
+        candidate_list.delete(last["candidate_id"].to_i)
+        winner_id = records_array.to_a[0]["candidate_id"].to_i
+      end
+
+      return winner_id
     end
 end
