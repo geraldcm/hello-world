20fd4f3795b0eeb7d75ee8267e5051cd6f6e68fa
Gerald Coulter-Mistele <gerald@prosperworks.com>
2017-01-24 20:10:18 -0800

    fix bug

--- lib/logic/logic.rb
+++ lib/logic/logic.rb
@@ -2,7 +2,7 @@ class Logic
   def self.start_vote(params)
     poll = Poll.where(name: poll_name).order(:created_at)
     if(p.length == 0)
-      return = {
+      return {
         "response_type" =>  "ephemeral",
         "text" => "no poll found with #{poll_name}"
       }
